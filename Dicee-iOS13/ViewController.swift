b7b3ddfe5b2f477b2a5c0011fcc0aa13d86d311f
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-04-02 14:32:19 +0300

    final

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -7,18 +7,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    //'viewDidLoad' is when u open app and what u see(result)
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //WHO          WHAT    VALUE
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-//        diceImageView1.alpha = 0.5
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-    }
+    
+    
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement() //first way
+        diceImageView2.image = diceArray[Int.random(in: 0...5)] //second way
+        
+        
     }
 }
 
